76e6d5789e8f36e0815fea2aad5f9ba29739ca6a
EmreCakmakQA <ECakmak@qa.com>
2020-11-09 11:37:53 +0000

    init commit

---
+++ src/main/java/com/qa/community/javabeginner/conditionals/UniqueSum.java
@@ -0,0 +1,17 @@
+package com.qa.community.javabeginner.conditionals;
+
+public class UniqueSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//HAVE NOT FINISHED THIS CHALLENGE YET!!
+	}
+
+	public static int uniqueSum(int one, int two, int three) {
+		int result = 0;
+		if(one != two && one != three && two != three) {
+			result = one + two + three;
+		}
+		return result;
+	}
+}
---
+++ src/main/java/com/qa/community/javabeginner/iteration/Q1.java
@@ -0,0 +1,14 @@
+package com.qa.community.javabeginner.iteration;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Loops from 100 - 200 and prints to the console
+		for(int i = 100; i < 201; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
